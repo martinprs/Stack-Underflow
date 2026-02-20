d77022cfd6ba1bf16a34f9605cf5719e83d0a2d1
Martin Pruus <martin.pruus@tptlive.ee>
2026-02-20 19:04:15 +0200

    Switch from Vanilla to VueJS

---
+++ src/pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+        <h1>Hello Home page!</h1>
+    </main>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "../pages/home.vue";
+
+const router = createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{ path: "/", component: Home },
+	],
+});
+
+export default router;
