a97106056b9c0fef0bbc8d5adcaa473feaeae5b8
Martin Pruus <martinprs@users.noreply.github.com>
2026-08-26 22:21:30 +0300

    Add initial navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
+<script setup>
+import navbar from './components/navbar.vue';
+</script>
+
 <template>
     <main>
+        <navbar></navbar>
         <RouterView />
     </main>
 </template>
---
+++ src/components/navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Stack Underflow</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">For Teams</a>
+                    </li>
+                    <li class="nav-item">
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search..."
+                                aria-label="Search" />
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
