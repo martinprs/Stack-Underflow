57adfb17d29d611e6652b28c2cb7b631ae736797
Martin Pruus <martinprs@users.noreply.github.com>
2026-08-27 18:23:14 +0300

    Add initial navbar dropdown menu

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -8,7 +8,43 @@
                         <a class="nav-link" href="#">About</a>
                     </li>
                     <li class="nav-item">
-                        <button class="nav-link">Products</button>
+                        <div class="dropdown">
+                            <button class="nav-link" type="button" data-bs-toggle="dropdown">
+                                Products
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item" href="#">
+                                        <p class="dropdown_title">Stack Internal</p>
+                                        <p class="dropdown_caption">Implement a knowledge platform layer to power your enterprise and AI tools.</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">
+                                        <p class="dropdown_title">Stack Data Licensing</p>
+                                        <p class="dropdown_caption">Get access to top-class technical expertise with trusted & attributed content.</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">
+                                        <p class="dropdown_title">Stack Ads</p>
+                                        <p class="dropdown_caption">Connect your brand to the world's most trusted technologist communities.</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">
+                                        <p class="dropdown_title">Releases</p>
+                                        <p class="dropdown_caption">Keep up to-date on features we add to Stack Underflow and Stack Internal.</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">
+                                        <p class="dropdown_title">About the company</p>
+                                        <p class="dropdown_title">Visit the blog</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">For Teams</a>
@@ -39,5 +75,16 @@
 .nav-item:hover {
     background-color: #e3e6e8;
     border-radius: 360px;
+}
+
+.dropdown_title {
+    font-size: 13px;
+    color: black;
+}
+
+.dropdown_caption {
+    font-size: 12px;
+    font-weight: 400;
+    color: rgba(0, 0, 0, 0.65);
 }
 </style>
