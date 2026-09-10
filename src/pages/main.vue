756a70fe13ca8c37fabc7a699e72d29ad6933a11
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-10 16:46:54 +0300

    Add columns

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,5 +1,33 @@
 <template>
-    <main>
-        <h1>Hello Main page!</h1>
-    </main>
+    <div class="container text-center">
+        <div class="row custom-row">
+            <div class="first-column">
+                First column
+            </div>
+
+            <div class="second-column">
+                Second column
+            </div>
+        </div>
+    </div>
 </template>
+
+<style>
+.custom-row {
+    width: 1264px;
+    margin-left: 0;
+    margin-right: 0;
+}
+
+.first-column {
+    flex: 0 0 164px;
+    width: 164px;
+    padding: 0;
+}
+
+.second-column {
+    flex: 0 0 1100px;
+    width: 1100px;
+    padding: 0;
+}
+</style>
