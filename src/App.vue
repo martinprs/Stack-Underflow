b68848d42e02aaa2a52d965e53e8ed88a165ff0e
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-12 21:10:53 +0300

    Use double quates

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import navbar from './components/navbar.vue';
-import sidebar from './components/sidebar.vue';
+import navbar from "./components/navbar.vue";
+import sidebar from "./components/sidebar.vue";
 </script>
 
 <template>
