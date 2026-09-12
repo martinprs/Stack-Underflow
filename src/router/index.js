917694cba025c3165c08384dbf5d66fba3dd5a39
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-12 21:10:27 +0300

    Rename main page back to home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Main from "../pages/main.vue";
+import Home from "../pages/home.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
-		{ path: "/", component: Main },
+		{ path: "/", component: Home },
 	],
 });
 
