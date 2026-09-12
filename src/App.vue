4bc385198fbf084945353f477d8cfb38906edbf4
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-12 21:08:29 +0300

    Move sidebar to App.vue and simplifying main.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,41 @@
 <script setup>
 import navbar from './components/navbar.vue';
+import sidebar from './components/sidebar.vue';
 </script>
 
 <template>
     <main>
-        <navbar></navbar>
-        <RouterView />
+        <navbar />
+        <div class="container text-center">
+            <div class="row">
+                <div class="col first">
+                    <sidebar />
+                </div>
+
+                <div class="col second">
+                    <RouterView />
+                </div>
+            </div>
+        </div>
     </main>
 </template>
+
+<style scoped>
+.row {
+    width: 1264px;
+    margin: 0 0px;
+    height: 100vh;
+}
+
+.col.first {
+    border-right: 1px solid hsl(210, 8%, 85%);
+    flex: 0 0 164px;
+    width: 164px;
+    padding: 0px;
+}
+
+.col.second {
+    width: 1100px;
+    padding: 0px;
+}
+</style>
--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,37 +1,3 @@
-<script setup>
-import sidebar from  "../components/sidebar.vue";
-</script>
-
 <template>
-    <div class="container text-center">
-        <div class="row">
-            <div class="col first">
-                <sidebar />
-            </div>
-
-            <div class="col second">
-                Second column
-            </div>
-        </div>
-    </div>
+    <p>Home!</p>
 </template>
-
-<style scoped>
-.row {
-    width: 1264px;
-    margin: 0 0px;
-    height: 100vh;
-}
-
-.col.first {
-    border-right: 1px solid hsl(210, 8%, 85%);
-    flex: 0 0 164px;
-    width: 164px;
-    padding: 0px;
-}
-
-.col.second {
-    width: 1100px;
-    padding: 0px;
-}
-</style>
