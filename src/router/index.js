c4180c5148776b54f8839a761cb73d1ce5c8d707
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-12 21:19:45 +0300

    Add initial questions page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../pages/home.vue";
+import Questions from "../pages/questions.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{ path: "/", component: Home },
+		{ path: "/questions", component: Questions },
 	],
 });
 
