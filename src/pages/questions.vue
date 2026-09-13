1d9dac849f95fe15ddf6a4d8f978375bc009a530
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-13 19:45:12 +0300

    Add initial layout

--- src/pages/questions.vue
+++ src/pages/questions.vue
@@ -1,3 +1,29 @@
 <template>
-    <p>Questions!</p>
+    <div class="container">
+        <div class="row">
+            <div class="col first">
+                First col
+            </div>
+            <div class="col second">
+                Second col
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.container {
+    padding: 24px;
+}
+
+.col.first {
+    flex: 0 0 727px;
+    padding: 0;
+}
+
+.col.second {
+    flex: 0 0 300px;
+    padding: 0;
+}
+
+</style>
